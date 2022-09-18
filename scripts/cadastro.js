bd7f424e3de9dbf089a17417668fc8355ff7bc5d
libanoaugusto <110346130+libanoaugusto@users.noreply.github.com>
2022-09-18 15:12:44 -0300

    correção cadastro

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -2,11 +2,11 @@ var lista_time =[]
 
 function salvar() {
     let nome_time = document.getElementById("nome").value
-    let escudo_time = document.getElementById("escudo").value
-    let contato_time = document.getElementById("contato").value
-    let cor_time = document.getElementById("cor").value
-    let endereco_campo = document.getElementById("endereco").value
-    let horario_partida = document.getElementById("horario").value
+    let escudo_time = document.getElementById("escudo_time").value
+    let contato_time = document.getElementById("contato_time").value
+    let cor_time = document.getElementById("cor_uniforme").value
+    let endereco_campo = document.getElementById("endereco_campo").value
+    let horario_partida = document.getElementById("horario_partida").value
 
     let time = {
         nome: nome_time,
